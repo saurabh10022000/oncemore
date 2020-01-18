98420f66e441c2a8ceebd4278d6cc22ac11e9d1f
Saurabh jain <saurabhjain4916@gmail.com>
2020-01-19 00:07:12 +0530

    Updates

---
+++ precache-manifest.8a200aaf1b7f4bd8e74db776473afae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b664e0739514725bf6e06cc790107",
+    "url": "/oncemore/index.html"
+  },
+  {
+    "revision": "f9fe7ed0277ef665a193",
+    "url": "/oncemore/static/css/main.a866365c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/oncemore/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/oncemore/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9fe7ed0277ef665a193",
+    "url": "/oncemore/static/js/main.2f890109.chunk.js"
+  },
+  {
+    "revision": "ee40b5c71d0a32e0e089",
+    "url": "/oncemore/static/js/runtime-main.72173e69.js"
+  }
+]);
